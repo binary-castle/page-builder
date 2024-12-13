88844bcd2d2ba70dae94d3b28e048d76aaa90920
Ekramul Islam Sumon <38817030+sumoninfo@users.noreply.github.com>
2024-12-14 00:03:53 +0600

    Merge/conflict issue fixed

--- src/lib/PageBuilder.ts
+++ src/lib/PageBuilder.ts
@@ -1,26 +1,31 @@
 import {ref, Ref} from "vue";
+import {v4 as uuidv4} from 'uuid';
 import {ButtonBlock} from "./utils/blocks/ButtonBlock.ts";
 import {ColumnBlock} from "./utils/blocks/ColumnBlock.ts";
-import {ListBlock} from "./utils/blocks/ListBlock.ts";
+// import {ListBlock} from "./utils/blocks/ListBlock.ts";
 import {Block} from "./utils/types.ts";
 import {HtmlBlock} from "./utils/blocks/HtmlBlock.ts";
 import {TextBlock} from "./utils/blocks/TextBlock.ts";
+import {CardUiComponent} from "./utils/ui-components/CardUiComponent.ts";
 
 const draggedItem: Ref<Block | null> = ref(null)
 const renderList: Ref<Array<Block>> = ref([])
 const meta: Ref<Array<Record<string, string>>> = ref([])
+const activeComponent: Ref<String | null> = ref('MetaComponent')
 const dragOverIndex: Ref<number | null> = ref(null)
 const dragOverDropZone: Ref<boolean> = ref(false)
 const innerDragElement: Ref<Block | null> = ref(null)
 const innerDragElementIndex: Ref<number | null> = ref(null)
-const activeComponent: Ref<String | null> = ref('MetaComponent')
+const selectedOptionComponent: Ref<Block | null> = ref(null)
+const dragOverChildElement: Ref<boolean> = ref(false)
 
 const blocks: Ref<Array<Block>> = ref([
     new ButtonBlock(),
     new TextBlock(),
     new ColumnBlock(),
-    new ListBlock(),
-    new HtmlBlock()
+    // new ListBlock(),
+    new HtmlBlock(),
+    new CardUiComponent(),
 ])
 
 export function usePageBuilder() {
@@ -38,6 +43,7 @@ export function usePageBuilder() {
             }
 
             if (dragOverIndex.value === null) {
+                innerDragElement.value.id = uuidv4()
                 renderList.value.push(innerDragElement.value)
             } else {
                 renderList.value.splice(dragOverIndex.value, 0, innerDragElement.value);
@@ -49,6 +55,7 @@ export function usePageBuilder() {
             const droppedItem = event.dataTransfer?.getData('text/plain');
             if (droppedItem) {
                 const _droppedItem = JSON.parse(droppedItem);
+                _droppedItem.id = uuidv4();
                 if (dragOverIndex.value === null) {
                     renderList.value.push(_droppedItem)
                 } else {
@@ -61,11 +68,20 @@ export function usePageBuilder() {
 
     }
 
-    // const onDragEnterZone
+    const onDropChildElement = () => {
+        if (innerDragElementIndex.value != null) {
+            renderList.value.splice(innerDragElementIndex.value, 1)
+        }
+        draggedItem.value = null;
+        dragOverIndex.value = null;
+        innerDragElement.value = null;
+        dragOverDropZone.value = false;
+        dragOverChildElement.value = true;
+        innerDragElementIndex.value = null;
+    }
+
 
     const onDragLeave = () => {
-        // console.log('onDragLeave', $event)
-        // dragOverIndex.value = null;
         dragOverDropZone.value = false;
         dragOverIndex.value = null;
     }
@@ -73,45 +89,95 @@ export function usePageBuilder() {
     const startDragItem = ($event: DragEvent, item: Block, index: number) => {
         innerDragElement.value = item
         innerDragElementIndex.value = index
-        // renderList.value.splice(index, 1)
-        // console.log(item);
-        // console.log(index)
-        // $event.dataTransfer?.setData('text/plain', JSON.stringify(item));
+        $event.dataTransfer?.setData('text/plain', JSON.stringify(item));
     }
 
     const onDragOverItem = ($event: DragEvent, index: number) => {
-        console.log('on drag over parent')
         $event.preventDefault();
         $event.stopPropagation();
-        // console.log('onDragOverItem', $event)
         dragOverIndex.value = index;
     }
 
     const onDragOver = ($event: DragEvent) => {
         dragOverIndex.value = null;
         dragOverDropZone.value = true;
-        // console.log("on drag over");
         $event.stopPropagation();
+    }
+
+    const onDragOverChildElement = (value: boolean) => {
+        dragOverChildElement.value = value
+    }
+
+    const onItemSelect = (block: Block) => {
+        selectedOptionComponent.value = block
+    }
+
+    const onSelectFormChildElement = (block: Block) => {
+        if (block) {
+            selectedOptionComponent.value = block
+        }
+    }
+
+    const onDelete = ($event: boolean) => {
+        if (selectedOptionComponent.value?.id && $event) {
+            const index = renderList.value.findIndex((block) => block.id === selectedOptionComponent.value?.id);
+            if (index !== -1) {
+                renderList.value.splice(index, 1);
+                selectedOptionComponent.value = null
+                return
+            }
+
+            renderList.value.forEach((block: Block) => {
+                if (block.children) {
+                    if (Array.isArray(block.children)) {
+                        const childIndex = block.children.findIndex((block) => block.id === selectedOptionComponent.value?.id);
+                        if (childIndex !== -1) {
+                            block.children.splice(childIndex, 1); // Remove the block
+                            selectedOptionComponent.value = null
+                            return true;
+                        }
+                    } else if (typeof block.children === 'object') {
+                        for (const key in block.children) {
+                            const childIndex = block.children[key].findIndex((block) => block.id === selectedOptionComponent.value?.id);
+                            if (childIndex !== -1) {
+                                block.children[key].splice(childIndex, 1); // Remove the block
+                                selectedOptionComponent.value = null
+                                return true;
+                            }
+                        }
+                    }
+                }
+            })
+        }
     }
 
     const setActiveComponent = (component: string) => {
         activeComponent.value = component;
     }
 
+
     return {
         blocks,
         renderList,
-        meta,
         draggedItem,
         dragOverIndex,
         dragOverDropZone,
-        activeComponent,
+        selectedOptionComponent,
+        dragOverChildElement,
         startDrag,
         onDrop,
+        onDropChildElement,
         startDragItem,
         onDragOverItem,
+        onDragOverChildElement,
         onDragOver,
         onDragLeave,
+        onItemSelect,
+        onSelectFormChildElement,
+        onDelete,
+        //
+        activeComponent,
+        meta,
         setActiveComponent
     }
 }
--- src/lib/layouts/SideBar.vue
+++ src/lib/layouts/SideBar.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
-
 import MetaComponent from "../components/MetaComponent.vue";
 import BuilderComponent from "../components/BuilderComponent.vue";
 import {usePageBuilder} from "../PageBuilder.ts";
+import {ref} from "vue";
 
 const {setActiveComponent, activeComponent} = usePageBuilder()
 
@@ -17,6 +17,21 @@ const getComponent = (name: string) => {
   return activeItem?.component || null;
 };
 
+const sideItems = ref({
+  'tags': false,
+  'blocks': true,
+  'components': false
+})
+
+const toggleSideItem = (key: keyof typeof sideItems.value): void => {
+  // Reset all items to false
+  for (const k in sideItems.value) {
+    sideItems.value[k as keyof typeof sideItems.value] = false;
+  }
+
+  // Enable the clicked item
+  sideItems.value[key] = true;
+};
 </script>
 
 <template>
@@ -31,9 +46,47 @@ const getComponent = (name: string) => {
     >
       <span :class="item.icon"></span>
     </div>
+
+    <!--    <div class="bc-page-builder&#45;&#45;main-sidebar&#45;&#45;item" :class="{ 'active': sideItems.tags }" role="button"
+             @click="toggleSideItem('tags')">
+          <span class="icon-tags"></span>
+        </div>
+        <div class="bc-page-builder&#45;&#45;main-sidebar&#45;&#45;item" :class="{ 'active': sideItems.blocks }" role="button"
+             @click="toggleSideItem('blocks')">
+          <span class="icon-box"></span>
+        </div>
+        <div class="bc-page-builder&#45;&#45;main-sidebar&#45;&#45;item" :class="{ 'active': sideItems.components }" role="button"
+             @click="toggleSideItem('components')">
+          <span class="icon-boxes"></span>
+        </div>-->
   </div>
 
   <div class="bc-page-builder--blocks-sidebar">
+
+    <!--    <div class="bc-page-builder&#45;&#45;blocks-sidebar&#45;&#45;blocks" v-if="sideItems.tags">
+          tags
+        </div>
+
+        <div class="bc-page-builder&#45;&#45;blocks-sidebar&#45;&#45;blocks" v-if="sideItems.blocks">
+          <div v-for="(item, index) of blockRegistry.filter(el => el.type == 'element')" draggable="true"
+               class="block"
+               @dragstart="startDrag($event, item)"
+               :key="`item_${index}`">
+            <div class="icon" v-html="item.icon"></div>
+            <div class="title">{{ item.title }}</div>
+          </div>
+        </div>
+
+        <div class="bc-page-builder&#45;&#45;blocks-sidebar&#45;&#45;blocks" v-if="sideItems.components">
+          <div v-for="(item, index) of blockRegistry.filter(el => el.type === 'ui-component')" draggable="true"
+               class="block"
+               @dragstart="startDrag($event, item)"
+               :key="`item_${index}`">
+            asd
+            <div class="icon" v-html="item.icon"></div>
+            <div class="title">{{ item.title }}</div>
+          </div>
+        </div>-->
     <component :is="getComponent(activeComponent)"/>
   </div>
 </template>
