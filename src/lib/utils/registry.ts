814de3f16b5bc1be0f9a469748365b307b0be3d5
K M Rifat ul alom <kmrifat@gmail.com>
2024-12-29 04:10:58 +0600

    fix(pencil): fix register block (#18)

--- src/lib/utils/registry.ts
+++ src/lib/utils/registry.ts
@@ -9,6 +9,12 @@ export const previewOptionMap: Ref<Record<string, VueComponent | null>> = ref({}
 
 // Function to register a new block
 export function registerBlock(block: Block) {
+    const isBlockRegistered = blockRegistry.value.some((registeredBlock) => registeredBlock.name === block.name);
+
+    if (isBlockRegistered) {
+        console.warn(`Block "${block.name}" is already registered.`);
+        return; // Exit early to prevent duplicate registration
+    }
     blockRegistry.value.push(block);
     previewComponentMap.value[block.name] = block.component;
     previewOptionMap.value[block.name] = block.optionComponent || null
